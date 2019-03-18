ee4e72e772195b94cec9e43aae66635ec6e47485
Ettien <Ettien@The-Red-King.3521.dhcp.asu.edu>
2019-03-18 11:22:33 -0700

    Final product

--- Assignment 3/src/cse360assign3/Main.java
+++ Assignment 3/src/cse360assign3/Main.java
@@ -18,17 +18,14 @@ public class Main
 		//list.print();
 		System.out.println(list.toString());
 		
-		//System.out.println("The current size of the ");
 		System.out.println("\nThe current size of the array is: " + list.size());
 		System.out.println("\nThe current length of the array is: " + list.length() + "\n");
 		
 		list.delete(8);
-		list.delete(10);
 		list.delete(2);
 		list.delete(5);
 		list.delete(4);
 		
-		//System.out.println(list);
 		//list.print();
 		System.out.println(list.toString());
 		
--- Assignment 3/testsrc/cse360assign3/OrderedIntListTest.java
+++ Assignment 3/testsrc/cse360assign3/OrderedIntListTest.java
@@ -3,48 +3,40 @@ package cse360assign3;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class OrderedIntListTest {
-
+	
+	/**
+	 * initializes the array test which will be used for JUNIT
+	 * to test each individual method
+	 */
 	private OrderedIntList test = new OrderedIntList();
 	
 	@Test
 	public void testOrderedIntList() {
-		//fail("Not yet implemented");
-		//test = new OrderedIntList();
-		//assertEquals(10,10);
-		assertEquals("wrong array", 10, test.size());
+		assertEquals("array size", 10, test.size());
 	}
 
-//	@Test
-//	public void testConstructor() {
-//		//fail("Not yet implemented");
-//		test = new OrderedIntList(9);
-//		//assertNotEquals(9,8);
-//		assertEquals ("wrong array", 8, test.size());
-//	}
-
-	/*@Test
+	@Test
 	public void testOrderedIntListInt() {
-		fail("Not yet implemented");
-	}*/
+		test = new OrderedIntList(4);
+		assertEquals("array size", 4, test.size());
+	}
 
 	@Test
 	public void testInsert() {
-		System.out.println("\nTesting Insert method");
-		//test = new OrderedIntList();
 		assertEquals(10, test.size());
+		//insert values into array to check insert method functionality 
 		test.insert(2);
 		test.insert(3);
 		test.insert(4);
 		test.insert(5);
 		test.insert(6);
 		test.insert(7);
-		assertEquals(10, test.size());
+		assertEquals(6, test.length());
 		assertEquals("# of elements", 6, 6);
-		System.out.println(test + " ");
+		//Repeat process to confirm insert method functionality 
 		test.insert(8);
 		test.insert(9);
 		test.insert(10);
@@ -52,38 +44,29 @@ public class OrderedIntListTest {
 		test.insert(12);
 		test.insert(13);
 		assertEquals("# of elements", 12, 12);
-		System.out.println(test + " ");
 		assertEquals(15, test.size());
-		//fail("Not yet implemented");
 	}
 	
-//	@Test
-//	public void testSize() {
-//		test = new OrderedIntList();
-//		assertEquals(test.size(), 10);
-//		test.insert(2);
-//		assertEquals(test.size(), 15);
-//
-//		//fail("Not yet implemented");
-//	}
+	@Test
+	public void testSize() {
+		test = new OrderedIntList();
+		assertEquals(test.size(), 10);
+		test.insert(2);
+		assertEquals(test.size(), 10);
+	}
 
 	@Test
 	public void testLength() {
-		System.out.println("\nTesting Length method");
 		test.insert(2);
 		test.insert(11);
-		assertEquals("length", 2, 2);
-		System.out.println(test + " ");
+		assertEquals("length", test.length(), 2);
 		test.insert(9);
 		test.insert(14);
-		assertEquals("length", 4, 4);
-		System.out.println(test + " ");
-		//fail("Not yet implemented");
+		assertEquals("length", test.length(), 4);
 	}
 
 	@Test
 	public void testDelete() {
-		System.out.println("\nTesting Delete method");
 		test.insert(2);
 		test.insert(9);
 		test.insert(10);
@@ -97,13 +80,11 @@ public class OrderedIntListTest {
 		System.out.println(test + " ");
 
 		
-		//fail("Not yet implemented");
 		
 	}
 
 	@Test
 	public void testToString() {
-		//fail("Not yet implemented");
 		String stringIntList = "" + test;
 		assertEquals(stringIntList,test.toString());
 		
